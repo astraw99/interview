2437858ed4dc8c7e6123f2e030b9530d54c75a9b
astraw99 <wangchengiscool@gmail.com>
2019-12-01 16:14:06 +0800

    update bitwise

--- go/bitwise/test.go
+++ go/bitwise/test.go
@@ -4,6 +4,10 @@ import "fmt"
 
 func main() {
 	var d uint = 2
-	fmt.Printf("%b\n", ^d)
+	fmt.Printf("%b\n", ^d) // ^ 一元运算：按位取反
 	fmt.Println(len(fmt.Sprintf("%b", ^d)))
+
+	fmt.Println(^uintptr(0)) // ^ 一元运算：按位取反
+
+	fmt.Println(5 ^ 3) // 二元运算：异或xor: 101(5) xor 011(3) = 110(6)
 }
