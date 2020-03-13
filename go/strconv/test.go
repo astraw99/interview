fd061a8ae8699b6668f9dc904316a534fe3b3f91
astraw99 <wangchengiscool@gmail.com>
2020-03-13 16:28:54 +0800

    update strconv.Atoi

--- go/strconv/test.go
+++ go/strconv/test.go
@@ -6,9 +6,16 @@ import (
 )
 
 func main() {
-	str := "abc2"
+	str := "115.2"
+	// [核心] Atoi 转浮点数会报错，返回0
 	pageNumInt, err := strconv.Atoi(str)
 
-	// 0 strconv.Atoi: parsing "abc2": invalid syntax
+	// 0 strconv.Atoi: parsing "115.2": invalid syntax
 	fmt.Println(pageNumInt, err)
+
+	strFloat := "abc2"
+	float, err := strconv.ParseFloat(strFloat, 64)
+
+	// 0 strconv.Atoi: parsing "abc2": invalid syntax
+	fmt.Println(float, err)
 }
