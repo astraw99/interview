935f198a3c788041b00c19855af7702b08e8855a
astraw99 <wangchengiscool@gmail.com>
2021-06-07 17:25:11 +0800

    add runtime.Callers

--- go/runtime/caller.go
+++ go/runtime/caller.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"runtime"
+	"strings"
 )
 
 func main() {
@@ -14,6 +15,7 @@ func Foo() {
 }
 func Bar() {
 	fmt.Printf("我是 %s, %s 又在调用我!\n", printMyName(), printCallerName())
+	callers()
 }
 func printMyName() string {
 	pc, file, line, ok := runtime.Caller(1)
@@ -24,4 +26,37 @@ func printCallerName() string {
 	pc, file, line, ok := runtime.Caller(2)
 	fmt.Println(file, line, ok)
 	return runtime.FuncForPC(pc).Name()
+}
+
+func callers() {
+	pcs := make([]uintptr, 2)
+	_ = runtime.Callers(0, pcs)
+
+	var loggerPackage string
+	fmt.Println(runtime.Version())
+
+	if runtime.Version() >= "go1.14" {
+		loggerPackage = getPackageName(runtime.FuncForPC(pcs[0]).Name())
+	} else {
+		loggerPackage = getPackageName(runtime.FuncForPC(pcs[1]).Name())
+	}
+
+	fmt.Println("loggerPackage:", loggerPackage)
+}
+
+// getPackageName to get the package name
+func getPackageName(f string) string {
+	for {
+		lastPeriod := strings.LastIndex(f, ".")
+		lastSlash := strings.LastIndex(f, "/")
+
+		// [abc/def].main
+		if lastPeriod > lastSlash {
+			f = f[:lastPeriod]
+		} else {
+			break
+		}
+	}
+
+	return f
 }
