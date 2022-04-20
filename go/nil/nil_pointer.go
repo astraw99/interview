4adcab47db2f007f0c439c7b9fc731dd73a545de
astraw99 <wangchengiscool@gmail.com>
2022-04-20 10:19:26 +0800

    add nil struct pointer

---
+++ go/nil/nil_pointer.go
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2022 https://github.com/astraw99. All rights reserved.
+ *
+ * Licensed under the BSD 3-Clause License (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the
+ * License at
+ *
+ * https://opensource.org/licenses/BSD-3-Clause
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package main
+
+import "fmt"
+
+type myStruct struct {
+	Name string
+	Age  int
+}
+
+func main() {
+	var s myStruct
+
+	fmt.Printf("%T\n", s)
+	fmt.Println(s == nil) // zero struct is not nil
+
+	var s2 *myStruct
+
+	fmt.Printf("=====%T\n", s2)
+	fmt.Printf("%#v\n", s2)
+	fmt.Println(s2 == nil) // zero pointer struct is nil
+}
