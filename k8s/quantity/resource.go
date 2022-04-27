040ae6f64c315ac70a0bcca750f293de776fe741
astraw99 <wangchengiscool@gmail.com>
2022-04-27 11:02:26 +0800

    update k8s quantity

--- k8s/quantity/resource.go
+++ k8s/quantity/resource.go
@@ -30,16 +30,39 @@ func main() {
 	}
 
 	requests2 := corev1.ResourceList{
-		corev1.ResourceCPU:    resource.MustParse("100.1m"),
+		corev1.ResourceCPU:    resource.MustParse("10.1m"),
 		corev1.ResourceMemory: resource.MustParse("0.1Gi"),
 	}
 
-	fmt.Println(requests.Cpu().MilliValue())  // Cpu.Value is decimal = 1000
-	fmt.Println(requests2.Cpu().MilliValue()) // Cpu.Value is decimal = 100
-	fmt.Println(requests.Cpu().Value())       // Cpu.Value is decimal = 1
-	fmt.Println(requests2.Cpu().Value())      // Cpu.Value is decimal = 1
+	requests3 := corev1.ResourceList{
+		corev1.ResourceCPU:    resource.MustParse("1u"),
+		corev1.ResourceMemory: resource.MustParse("1u"),
+	}
 
+	requests4 := corev1.ResourceList{
+		//corev1.ResourceCPU: resource.MustParse("1t"), // panic: '^([+-]?[0-9.]+)([eEinumkKMGTP]*[-+]?[0-9]*)$'
+		corev1.ResourceCPU:    resource.MustParse("1n"),
+		corev1.ResourceMemory: resource.MustParse("1n"),
+	}
+
+	fmt.Println("request1================")
+	fmt.Println(requests.Cpu().Value())                         // Cpu.Value is decimal = 1
+	fmt.Println(requests.Cpu().MilliValue())                    // Cpu.Value is decimal = 1000
 	fmt.Println(requests.Memory().Value())                      // Memory.Value is byte = 1073741824
-	fmt.Println(requests2.Memory().Value())                     // Memory.Value is byte = 107374183
 	fmt.Println(requests.Memory().Value() / 1024 / 1024 / 1024) // = 1G
+
+	fmt.Println("request2================")
+	fmt.Println(requests2.Cpu().Value())      // Cpu.Value is decimal = 1
+	fmt.Println(requests2.Cpu().MilliValue()) // Cpu.Value is decimal = 11
+	fmt.Println(requests2.Memory().Value())   // Memory.Value is byte = 107374183
+
+	fmt.Println("request3================")
+	fmt.Println(requests3.Cpu().Value())      // Cpu.Value is decimal = 1
+	fmt.Println(requests3.Cpu().MilliValue()) // Cpu.Value is decimal = 1
+	fmt.Println(requests3.Memory().Value())   // Memory.Value is byte = 1
+
+	fmt.Println("request4================")
+	fmt.Println(requests4.Cpu().Value())      // Cpu.Value is decimal = 1
+	fmt.Println(requests4.Cpu().MilliValue()) // Cpu.Value is decimal = 1
+	fmt.Println(requests4.Memory().Value())   // Memory.Value is byte = 1
 }
